60da133a4d874e160c15806baed9b4b21f5ef771
Muhammad Warisi <mw.a.seven.one@gmail.com>
2024-12-02 13:12:10 +0700

    add collaborator fix

--- app/Models/UsersHasTeam.php
+++ app/Models/UsersHasTeam.php
@@ -9,6 +9,8 @@ class UsersHasTeam extends Model
 {
     use HasFactory;
     public $timestamps = true;
+
+    protected $primaryKey = 'user_id';
     protected $fillable = [
         'user_id',
         'project_id',
